a6ea144aea6bc17f9f3189e6af11b7bf2f8f2ddd
h2-heo <h0h6h2h5@gmail.com>
2020-10-20 00:04:37 +0900

    Implement Point

--- src/Point.kt
+++ src/Point.kt
@@ -1,5 +1,9 @@
-class Point(row: Int, col: Int) {
+data class Point(val row: Int, val col: Int) {
     fun moveTo(direction: Direction): Point {
-        TODO("Not yet implemented")
+        return when (direction) {
+            Direction.LEFT_DOWN -> Point(row + 1, col)
+            Direction.RIGHT -> Point(row, col + 1)
+            Direction.LEFT_UP -> Point(row - 1, col - 1)
+        }
     }
 }
