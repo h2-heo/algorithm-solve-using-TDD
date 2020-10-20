f7581e4e943e3eb9dc791498b540326e200f457b
h2-heo <h0h6h2h5@gmail.com>
2020-10-21 02:00:12 +0900

    Implement QuadTreeMatrixParser

--- src/QuadTreeMatrixParser.kt
+++ src/QuadTreeMatrixParser.kt
@@ -1,6 +1,28 @@
 class QuadTreeMatrixParser {
 
     fun parse(matrix: Array<IntArray>): QuadTree {
-        TODO("Not yet implemented")
+        val wholeRect = Rect.of(matrix)
+        return parse(matrix, wholeRect)
     }
+
+    private fun parse(matrix: Array<IntArray>, rect: Rect): QuadTree {
+        return if (matrix canCompressIn rect) {
+            val value = matrix.getAnyValueIn(rect)
+            QuadTree.create(value)
+        } else {
+            val (r0, r1, r2, r3) = rect.splitQuad()
+            QuadTree.create(
+                    parse(matrix, r0), parse(matrix, r1),
+                    parse(matrix, r2), parse(matrix, r3),
+            )
+        }
+    }
+}
+
+private fun <T> Array<T>.getAnyValueIn(rect: Rect): Int {
+    TODO("Not yet implemented")
+}
+
+private infix fun Array<IntArray>.canCompressIn(rect: Rect): Boolean {
+    TODO("Not yet implemented")
 }
---
+++ src/Rect.kt
@@ -0,0 +1,11 @@
+class Rect {
+    fun splitQuad(): List<Rect> {
+        TODO("Not yet implemented")
+    }
+
+    companion object {
+        fun of(matrix: Array<IntArray>): Rect {
+            TODO("Not yet implemented")
+        }
+    }
+}
