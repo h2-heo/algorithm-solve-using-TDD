61fa6502eab565d0e9a93e4ff19a00fa6250ebd4
h2-heo <h0h6h2h5@gmail.com>
2020-10-21 21:03:27 +0900

    Add QuadTree test

--- test/QuadTreeTest.kt
+++ test/QuadTreeTest.kt
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.assertThrows
 
 internal class QuadTreeTest {
 
@@ -102,5 +103,60 @@ internal class QuadTreeTest {
         ))
 
         assertEquals(result, tree.countElements())
+    }
+
+    @Test
+    fun `when creating by value, only values 0 and 1 are allowed`() {
+        assertDoesNotThrow { QuadTree.create(0) }
+        assertDoesNotThrow { QuadTree.create(1) }
+        assertThrows<IllegalArgumentException> { QuadTree.create(-1) }
+        assertThrows<IllegalArgumentException> { QuadTree.create(2) }
+    }
+
+    @Test
+    fun `when created by value, can access value`() {
+        assertEquals(0, QuadTree.create(0).value)
+        assertEquals(1, QuadTree.create(1).value)
+    }
+
+    @Test
+    fun `when created by value, should be leaf`() {
+        val tree = QuadTree.create(0)
+
+        assertTrue(tree.isLeaf())
+    }
+
+    @Test
+    fun `when created by value, should not have child`() {
+        val tree = QuadTree.create(0)
+
+        assertEquals(0, tree.children.size)
+    }
+
+    @Test
+    fun `when created by subtrees, should not be leaf`() {
+        val tree = QuadTree.create(
+                QuadTree.create(0), QuadTree.create(1),
+                QuadTree.create(1), QuadTree.create(0),
+        )
+
+        assertFalse(tree.isLeaf())
+    }
+
+    @Test
+    fun `when created by subtrees, can access them`() {
+        val q0 = QuadTree.create(0)
+        val q1 = QuadTree.create(0)
+        val q2 = QuadTree.create(0)
+        val q3 = QuadTree.create(0)
+        val tree = QuadTree.create(
+                q0, q1,
+                q2, q3,
+        )
+
+        assertEquals(q0, tree.children[0])
+        assertEquals(q1, tree.children[1])
+        assertEquals(q2, tree.children[2])
+        assertEquals(q3, tree.children[3])
     }
 }
