92fa28ea2513e475054a79bf74de7d6cafb0532b
h2-heo <h0h6h2h5@gmail.com>
2020-10-29 22:52:39 +0900

    Implement SearchEngine

---
+++ src/main/PageMatcher.kt
@@ -0,0 +1,10 @@
+class PageMatcher {
+
+    fun addAll(pages: Iterable<WebPage>): PageMatcher {
+        TODO("Not yet implemented")
+    }
+
+    fun getMatchingScore(page: WebPage, word: String): Double {
+        TODO("Not yet implemented")
+    }
+}
--- src/main/SearchEngine.kt
+++ src/main/SearchEngine.kt
@@ -1,14 +1,24 @@
 class SearchEngine {
 
-    fun register(page: WebPage) {
-        TODO("Not yet implemented")
+    private val pages = arrayListOf<WebPage>()
+
+    fun register(page: WebPage): SearchEngine {
+        pages.add(page)
+        return this
     }
 
-    fun registerAll(pages: Iterable<WebPage>) {
+    fun registerAll(pages: Iterable<WebPage>): SearchEngine {
         pages.forEach { register(it) }
+        return this
     }
 
-    fun search(word: String): WebPage {
-        TODO("Not yet implemented")
+    fun search(word: String): WebPage? {
+        return pages.maxBy { getMatchingScore(it, word) }
+    }
+
+    private fun getMatchingScore(page: WebPage, word: String): Double {
+        return PageMatcher()
+                .addAll(pages)
+                .getMatchingScore(page, word)
     }
 }
