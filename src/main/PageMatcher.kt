e89ccdccff59467a5be0d830c01da5036375dae2
h2-heo <h0h6h2h5@gmail.com>
2020-10-29 23:40:14 +0900

    Add test

--- src/main/PageMatcher.kt
+++ src/main/PageMatcher.kt
@@ -1,10 +1,22 @@
 class PageMatcher {
 
+    fun add(page: WebPage) {
+        TODO("Not yet implemented")
+    }
+
     fun addAll(pages: Iterable<WebPage>): PageMatcher {
         TODO("Not yet implemented")
     }
 
     fun getMatchingScore(page: WebPage, word: String): Double {
+        TODO("Not yet implemented")
+    }
+
+    fun getBaseScore(page: WebPage, word: String): Double {
+        TODO("Not yet implemented")
+    }
+
+    fun getLinkScore(page: WebPage, word: String): Double {
         TODO("Not yet implemented")
     }
 }
